d736bfbcdb10ffe177c2502a094172ecab223d6a
Artur16211 <herrera.alvaradoartu@gmail.com>
2025-11-24 13:28:08 -0600

    fix

--- Atlas/navbar.js
+++ Atlas/navbar.js
@@ -1,10 +1,10 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
   const navbarHTML = `
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container-fluid justify-content-center">
       <ul class="navbar-nav">
         <li class="nav-item"><a class="nav-link" href="/Atlas.html">Atlas</a></li>
-        <li class="nav-item"><span class="text-secondary d-none d-md-inline">|</span></li>
+        <li class="nav-item"><span class="text-secondary d-none d-lg-inline">|</span></li>
         <li class="nav-item"><a class="nav-link" href="/Atlas/MW.html">MW</a></li>
         <li class="nav-item"><a class="nav-link" href="/Atlas/MWII.html">MWII</a></li>
         <li class="nav-item"><a class="nav-link" href="/Atlas/MWIII.html">MWIII</a></li>
