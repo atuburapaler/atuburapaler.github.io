9b9bd4f171e71b8064e69ce0d6eb53f0476e4ead
Artur16211 <herrera.alvaradoartu@gmail.com>
2025-11-23 21:43:50 -0600

    update

--- Atlas/navbar.js
+++ Atlas/navbar.js
@@ -3,6 +3,8 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm">
     <div class="container-fluid justify-content-center">
       <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="/Atlas.html">Atlas</a></li>
+        <li class="nav-item"><span class="text-secondary d-none d-md-inline">|</span></li>
         <li class="nav-item"><a class="nav-link" href="/Atlas/MW.html">MW</a></li>
         <li class="nav-item"><a class="nav-link" href="/Atlas/MWII.html">MWII</a></li>
         <li class="nav-item"><a class="nav-link" href="/Atlas/MWIII.html">MWIII</a></li>
@@ -10,6 +12,8 @@
       </ul>
     </div>
   </nav>
+  <br class="d-block d-md-none" />
+  <br class="d-block d-md-none" />
   <br class="d-block d-md-none" />
   <br class="d-block d-md-none" />
   <br class="d-block d-md-none" />
